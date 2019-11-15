b1a68c184d36fa19e2530e53e7041abfdfb6c864
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-11-15 15:03:18 +0900

    First import

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/username/qi/cmd"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "qiitasearch"
+	app.Usage = "search qiita articles"
+	app.Version = "0.1.0"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "myqi",
+			Usage: "qiita + mine : you get yours articles",
+			Action: func(c *cli.Context) error {
+				qiitaToken := os.Getenv("QIITA_TOKEN")
+				datas := cmd.FetchQiitaData(qiitaToken)
+				cmd.OutputQiitaData(datas)
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
